df693fa20325b04c1ded7b858b65105cc82b722c
rafayhanan <mrafayhanan@gmail.com>
2025-01-25 17:56:31 +0500

    sentiment analysis api working

--- Flask/sentiment-analysis/app.py
+++ Flask/sentiment-analysis/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-app = Flask(__name__)
+app = Flask("Sentiment Analyzer")
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid Input! Try Again!"
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
