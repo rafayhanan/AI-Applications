88a9a33aaac104ca7752cd20fc810939c6c1f336
rafayhanan <mrafayhanan@gmail.com>
2025-01-26 03:49:39 +0500

    sentiment analysis app complete

--- Flask/sentiment-analysis/SentimentAnalysis/sentiment_analysis.py
+++ Flask/sentiment-analysis/SentimentAnalysis/sentiment_analysis.py
@@ -1,19 +1,26 @@
 import requests
-import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("HUGGINGFACE_API_KEY")
+api_url = os.getenv("API_URL")
+headers = {"Authorization": f"Bearer {api_key}"}
 
 def sentiment_analyzer(text_to_analyze):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url,json=myobj,headers=header)
-    formatted_response = json.loads(response.text)
+    payload = { "inputs": text_to_analyze } 
+    response = requests.post(api_url,headers=headers,json=payload)
+    formatted_response = response.json()
     if response.status_code == 200:
-        label = formatted_response['documentSentiment']['label']
-        score = formatted_response['documentSentiment']['score']
+        label = formatted_response[0][0]['label']
+        score = formatted_response[0][0]['score']
     elif response.status_code == 500:
         label = None
         score = None
-    label = formatted_response["documentSentiment"]["label"]
-    score = formatted_response["documentSentiment"]["score"]
+    label = formatted_response[0][0]['label']
+    score = formatted_response[0][0]['score']
     return {'label':label,'score':score}
+   
+
+
 
